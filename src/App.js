f21eee22bc3899c32ba8151d322f59731a8d41fc
sandromio <sandromio@Dell-SandroRMio.localdomain>
2020-07-30 20:44:33 -0300

    setup inicial do projeto

--- src/App.js
+++ src/App.js
@@ -4,7 +4,41 @@ import Menu from './components/Menu'
 function App() {
   return (
     <div>
-      <Menu  />
+      <Menu/>
+
+      <BannerMain
+        videoTitle={dadosIniciais.categoria[0].videos[0].titulo}
+        url={dadosIniciais.categoria[0].videos[0].url}
+        videoDescription={"O que é Front-end trabalhando na area os termos HTML, CSS e JS fazem parte da rotina das desenvolvedoras e desenvolvedores, Mas o que eles fazem afinal descubra com a Vanessa "}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />
+
+      <footer />
+
     </div>
   );
 }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import Button from '../Button';
+
+
+// import ButtonLink from './components/ButtonLink';
 
 function Menu(){
     return (
-        <header>
-            Mioflix Showm!!
-        </header>
+        <nav className="Menu" >
+            <a href="/" >
+            <img className="Logo" src= {Logo}  alt="Mioflix logo" />
+            </a>
+
+            <Button as="a" className="ButtonLink" href="/" >
+                novo vídeo
+            </Button>
+        </nav>
     );
 }
 
